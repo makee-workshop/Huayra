a83477ad22daabb7040dcf8eb787f3d841654298
s890506 <s890506@gmail.com>
2019-01-23 23:43:23 +0800

    Modify react-scripts project directory structure and add some test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-require('dotenv').config()
+import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
+serviceWorker.unregister()
