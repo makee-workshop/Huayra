613f339263856838c8afb42d8529441c378d84b9
s890506 <s890506@gmail.com>
2019-02-14 00:03:17 +0800

    Fix admin layout navbar menu

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -6,13 +6,33 @@ import { connect } from 'react-redux'
 const year = new Date().getFullYear()
 
 class Default extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      navBarOpen: false
+    }
+  }
+
+  compomentDidUpdate () {
+    this.setState({ navBarOpen: false })
+  }
+
   tabClass (tab) {
     return ClassNames({
       active: this.props.children.props.location.pathname === tab
     })
   }
 
+  toggleMenu () {
+    this.setState({ navBarOpen: !this.state.navBarOpen })
+  }
+
   render () {
+    const navBarCollapse = ClassNames({
+      'navbar-collapse': true,
+      collapse: !this.state.navBarOpen
+    })
+
     return (
       <div>
         <div className='navbar navbar-inverse navbar-fixed-top'>
@@ -22,8 +42,13 @@ class Default extends Component {
                 <img className='navbar-logo' src='/media/logo-square.png' alt='' />
                 <span className='navbar-brand-label'>Huayra</span>
               </Link>
+              <button className='navbar-toggle collapsed' onClick={this.toggleMenu.bind(this)}>
+                <span className='icon-bar' />
+                <span className='icon-bar' />
+                <span className='icon-bar' />
+              </button>
             </div>
-            <div className='navbar-collapse collapse'>
+            <div className={navBarCollapse}>
               <ul className='nav navbar-nav'>
                 <li className={this.tabClass('/admin')}>
                   <Link to='/admin'> 首頁
