e244d162427d63b71d81870a42ff7354f8e9410b
s890506 <s890506@gmail.com>
2020-07-31 18:35:48 +0800

    Update home card display

--- src/home/home.js
+++ src/home/home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { Helmet } from 'react-helmet'
-import { Container, Row, Col, Jumbotron, Card, CardTitle, CardText } from 'reactstrap'
+import { Container, Row, Col, Jumbotron, Card, CardHeader, CardBody, CardText } from 'reactstrap'
 import './css/home.min.css'
 
 class index extends Component {
@@ -19,54 +19,60 @@ class index extends Component {
         </Jumbotron>
         <Row>
           <Col sm='4'>
-            <Card body>
-              <CardTitle>
-                <h4>關於我們</h4>
-              </CardTitle>
-              <CardText>
-                Bicycle rights jean shorts la croix
-                vexillologist hell of kitsch. Photo booth
-                craft beer fixie raw denim hot chicken.
-                Pickled adaptogen sartorial brooklyn tilde
-                bay area.
-              </CardText>
-              <Link to='/about' className='btn btn-info btn-block'>
-                瞭解更多
-              </Link>
+            <Card>
+              <CardHeader>
+                關於我們
+              </CardHeader>
+              <CardBody>
+                <CardText>
+                  Bicycle rights jean shorts la croix
+                  vexillologist hell of kitsch. Photo booth
+                  craft beer fixie raw denim hot chicken.
+                  Pickled adaptogen sartorial brooklyn tilde
+                  bay area.
+                </CardText>
+                <Link to='/about' className='btn btn-info btn-block'>
+                  瞭解更多
+                </Link>
+              </CardBody>
             </Card>
           </Col>
 
           <Col sm='4'>
-            <Card body>
-              <CardTitle>
-                <h4>註冊會員</h4>
-              </CardTitle>
-              <CardText>
-                Health goth skateboard tousled umami, tofu
-                squid organic freegan +1 keytar brunch
-                post-ironic. Copper mug selfies tattooed
-                chicharrones short ribs yolo cardigan.
-              </CardText>
-              <Link to='/signup' className='btn btn-success btn-block'>
-                瞭解更多
-              </Link>
+            <Card>
+              <CardHeader>
+                註冊會員
+              </CardHeader>
+              <CardBody>
+                <CardText>
+                  Health goth skateboard tousled umami, tofu
+                  squid organic freegan +1 keytar brunch
+                  post-ironic. Copper mug selfies tattooed
+                  chicharrones short ribs yolo cardigan.
+                </CardText>
+                <Link to='/signup' className='btn btn-success btn-block'>
+                  瞭解更多
+                </Link>
+              </CardBody>
             </Card>
           </Col>
 
           <Col sm='4'>
-            <Card body>
-              <CardTitle>
-                <h4>連絡我們</h4>
-              </CardTitle>
-              <CardText>
-                Roof party readymade tote bag hot chicken
-                blog. Fam readymade raclette hella quinoa.
-                Cred pour-over yuccie williamsburg shabby
-                chic ramps chartreuse messenger bag.
-              </CardText>
-              <Link to='/contact' className='btn btn-warning btn-block'>
-                瞭解更多
-              </Link>
+            <Card>
+              <CardHeader>
+                連絡我們
+              </CardHeader>
+              <CardBody>
+                <CardText>
+                  Roof party readymade tote bag hot chicken
+                  blog. Fam readymade raclette hella quinoa.
+                  Cred pour-over yuccie williamsburg shabby
+                  chic ramps chartreuse messenger bag.
+                </CardText>
+                <Link to='/contact' className='btn btn-warning btn-block'>
+                  瞭解更多
+                </Link>
+              </CardBody>
             </Card>
           </Col>
         </Row>
