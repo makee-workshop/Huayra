3e61adb6b99eb0c8209ae6d6adcae44d98e55232
Larry Lai <s890506@gmail.com>
2024-03-08 16:12:20 +0800

    Update not found page to function component

--- src/NotFoundPage.js
+++ src/NotFoundPage.js
@@ -1,18 +1,16 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 
-class NotFoundPage extends React.Component {
-  render () {
-    return (
-      <section className='container'>
-        <Helmet>
-          <title>沒有此頁面</title>
-        </Helmet>
-        <h1 className='page-header'>沒有此頁面</h1>
-        <p>本平台沒有此頁面。</p>
-      </section>
-    )
-  }
+const NotFoundPage = () => {
+  return (
+    <section className='container'>
+      <Helmet>
+        <title>沒有此頁面</title>
+      </Helmet>
+      <h1 className='page-header'>沒有此頁面</h1>
+      <p>本平台沒有此頁面。</p>
+    </section>
+  )
 }
 
 export default NotFoundPage
