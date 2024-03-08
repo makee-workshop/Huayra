2b3df26d7794656cc39620c2bb997294baa2b25d
Larry Lai <s890506@gmail.com>
2024-03-08 16:56:48 +0800

    Update account and admin home page to function component

--- src/account/home.js
+++ src/account/home.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import Moment from 'moment'
 import { Helmet } from 'react-helmet'
 import { Container, Row, Col, Card } from 'reactstrap'
 import styles from './css/account.module.css'
 
-class index extends Component {
-  constructor (props) {
-    super(props)
-    this.state = this.getThisMoment()
-  }
+const AccountHome = () => {
+  const [time, setTime] = useState(getThisMoment())
 
-  componentDidMount () {
-    this.interval = setInterval(this.refreshTime.bind(this), 1000)
-  }
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(getThisMoment())
+    }, 1000)
 
-  componentWillUnmount () {
-    clearInterval(this.interval)
-  }
+    return () => clearInterval(interval)
+  }, [])
 
-  refreshTime () {
-    this.setState(this.getThisMoment())
-  }
-
-  getThisMoment () {
+  function getThisMoment () {
     const thisMoment = Moment()
 
     return {
@@ -35,79 +28,65 @@ class index extends Component {
     }
   }
 
-  render () {
-    return (
-      <Container>
-        <Helmet>
-          <title>我的帳號</title>
-        </Helmet>
-        <Row>
-          <Col sm={7}>
-            <h1 className='page-header'>我的帳號</h1>
-            <div className='row'>
-              <Col sm={4}>
-                <Card body className='well text-center'>
-                  <div className={styles['stat-value']}>
-                    {this.state.hour}
-                  </div>
-                  <div className={styles['stat-label']}>時</div>
-                </Card>
-              </Col>
+  return (
+    <Container>
+      <Helmet>
+        <title>我的帳號</title>
+      </Helmet>
+      <Row>
+        <Col sm={7}>
+          <h1 className='page-header'>我的帳號</h1>
+          <div className='row'>
+            <Col sm={4}>
+              <Card body className={`well text-center ${styles['stat-card']}`}>
+                <div className={styles['stat-value']}>{time.hour}</div>
+                <div className={styles['stat-label']}>時</div>
+              </Card>
+            </Col>
 
-              <Col sm={4}>
-                <Card body className='well text-center'>
-                  <div className={styles['stat-value']}>
-                    {this.state.minute}
-                  </div>
-                  <div className={styles['stat-label']}>分</div>
-                </Card>
-              </Col>
+            <Col sm={4}>
+              <Card body className={`well text-center ${styles['stat-card']}`}>
+                <div className={styles['stat-value']}>{time.minute}</div>
+                <div className={styles['stat-label']}>分</div>
+              </Card>
+            </Col>
 
-              <Col sm={4}>
-                <Card body className='well text-center'>
-                  <div className={styles['stat-value']}>
-                    {this.state.second}
-                  </div>
-                  <div className={styles['stat-label']}>秒</div>
-                </Card>
-              </Col>
+            <Col sm={4}>
+              <Card body className={`well text-center ${styles['stat-card']}`}>
+                <div className={styles['stat-value']}>{time.second}</div>
+                <div className={styles['stat-label']}>秒</div>
+              </Card>
+            </Col>
 
-              <Col sm={4}>
-                <Card body className='well text-center'>
-                  <div className={styles['stat-value']}>
-                    {this.state.year}
-                  </div>
-                  <div className={styles['stat-label']}>年</div>
-                </Card>
-              </Col>
+            <Col sm={4}>
+              <Card body className={`well text-center ${styles['stat-card']}`}>
+                <div className={styles['stat-value']}>{time.year}</div>
+                <div className={styles['stat-label']}>年</div>
+              </Card>
+            </Col>
 
-              <Col sm={4}>
-                <Card body className='well text-center'>
-                  <div className={styles['stat-value']}>
-                    {this.state.month}
-                  </div>
-                  <div className={styles['stat-label']}>月</div>
-                </Card>
-              </Col>
+            <Col sm={4}>
+              <Card body className={`well text-center ${styles['stat-card']}`}>
+                <div className={styles['stat-value']}>{time.month}</div>
+                <div className={styles['stat-label']}>月</div>
+              </Card>
+            </Col>
 
-              <Col sm={4}>
-                <Card body className='well text-center'>
-                  <div className={styles['stat-value']}>
-                    {this.state.day}
-                  </div>
-                  <div className={styles['stat-label']}>日</div>
-                </Card>
-              </Col>
-            </div>
-          </Col>
-          <Col sm={5} className='text-center'>
-            <h1 className='page-header'>時間表</h1>
-            <i className='lnr lnr-chart-bars bamf' />
-          </Col>
-        </Row>
-      </Container>
-    )
-  }
+            <Col sm={4}>
+              <Card body className={`well text-center ${styles['stat-card']}`}>
+                <div className={styles['stat-value']}>{time.day}</div>
+                <div className={styles['stat-label']}>日</div>
+              </Card>
+            </Col>
+          </div>
+        </Col>
+        <Col sm={5} className='text-center'>
+          <h1 className='page-header'>時間表</h1>
+          <i className='lnr lnr-chart-bars bamf' />
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
-export default index
+export default AccountHome
--- src/admin/home.js
+++ src/admin/home.js
@@ -1,68 +1,55 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Container, Row, Col, Card } from 'reactstrap'
 import { Helmet } from 'react-helmet'
 import { get } from '../utils/httpAgent'
 import styles from './css/admin.module.css'
 
-class index extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      User: 0,
-      Admin: 0
-    }
+const AdminHome = () => {
+  const [userData, setUserData] = useState({ User: 0, Admin: 0 })
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = () => {
+    get('/1/admin/count').then((response) => {
+      if (response.success === true) {
+        setUserData({ ...response.data })
+      }
+    })
   }
 
-  componentDidMount () {
-    this.fetchData()
-  }
+  return (
+    <Container>
+      <Helmet>
+        <title>系統資訊</title>
+      </Helmet>
+      <Row>
+        <Col md={6}>
+          <h1 className='page-header'>系統資訊</h1>
+          <Row>
+            <Col md={6}>
+              <Card body inverse className={`dark-card text-center ${styles['stat-card']}`}>
+                <div className={styles['stat-value']}>{userData.User}</div>
+                <div className={styles['stat-label']}>使用者</div>
+              </Card>
+            </Col>
 
-  fetchData () {
-    get('/1/admin/count')
-      .then(r => {
-        if (r.success === true) {
-          this.setState({ ...r.data })
-        }
-      })
-  }
-
-  render () {
-    return (
-      <Container>
-        <Helmet>
-          <title>系統資訊</title>
-        </Helmet>
-        <Row>
-          <Col md={6}>
-            <h1 className='page-header'>系統資訊</h1>
-            <Row>
-              <Col md={6}>
-                <Card body inverse className='dark-card text-center'>
-                  <div className={styles['stat-value']}>
-                    {this.state.User}
-                  </div>
-                  <div className={styles['stat-label']}>使用者</div>
-                </Card>
-              </Col>
-
-              <Col md={6}>
-                <Card body inverse className='dark-card text-center'>
-                  <div className={styles['stat-value']}>
-                    {this.state.Admin}
-                  </div>
-                  <div className={styles['stat-label']}>管理者</div>
-                </Card>
-              </Col>
-            </Row>
-          </Col>
-          <Col md={6} className='text-center'>
-            <h1 className='page-header'>儀表板</h1>
-            <i className='lnr lnr-chart-bars bamf' />
-          </Col>
-        </Row>
-      </Container>
-    )
-  }
+            <Col md={6}>
+              <Card body inverse className={`dark-card text-center ${styles['stat-card']}`}>
+                <div className={styles['stat-value']}>{userData.Admin}</div>
+                <div className={styles['stat-label']}>管理者</div>
+              </Card>
+            </Col>
+          </Row>
+        </Col>
+        <Col md={6} className='text-center'>
+          <h1 className='page-header'>儀表板</h1>
+          <i className='lnr lnr-chart-bars bamf' />
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
-export default index
+export default AdminHome
