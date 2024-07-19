847ff0d088ad0e438383127180cdec950953e2af
Larry Lai <s890506@gmail.com>
2024-07-19 11:42:49 +0800

    Replace redux-devtools with reduxjs/toolkit

--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -1,11 +1,11 @@
-import { createStore, compose } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 
 const initialState = {
-  index: { // 主要資訊
-    authenticated: false, // 是否驗證
-    user: '', // 使用者名稱
-    email: '', // 使用者 email
-    role: '' // 使用者權限
+  index: {
+    authenticated: false,
+    user: '',
+    email: '',
+    role: ''
   }
 }
 
@@ -37,13 +37,9 @@ function rootReducer (state = initialState, action) {
   }
 }
 
-const enhancer = compose(
-  // Middleware you want to use in development:
-  // applyMiddleware(d1, d2, d3),
-  // Required! Enable Redux DevTools with the monitors you chose
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
-
-const store = createStore(rootReducer, initialState, enhancer)
+const store = configureStore({
+  reducer: rootReducer,
+  preloadedState: initialState
+})
 
 export default store
